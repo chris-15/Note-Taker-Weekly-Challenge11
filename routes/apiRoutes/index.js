c78d95eb9131883f2065a5059aefc5a4be846212
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-04-06 18:44:16 -0400

    added funciton to delete note and route for it

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,15 +2,14 @@ const fs = require("fs");
 const path = require("path");
 const router = require("express").Router();
 
-// require package for uuid which creates random unique id 
-const { v4: uuidv4 } = require('uuid');
+// require package for uuid which creates random unique id
+const { v4: uuidv4 } = require("uuid");
 
-
-const notes  = require("../../db/db");
+const notes = require("../../db/db");
 
 // get route that returns json data
 router.get("/notes", (req, res) => {
-    res.json(notes);
+  res.json(notes);
 });
 
 //function to create newNote and pushes to notes array in the json file
@@ -20,34 +19,58 @@ function createNewNote(body, notesArray) {
 
   fs.writeFileSync(
     path.join(__dirname, "../../db/db.json"),
-    JSON.stringify({ notes: notesArray}, null, 2)
+    JSON.stringify({ notes: notesArray }, null, 2)
   );
 
   return note;
 }
 
-// funcntion to validate input 
+// funcntion to validate input
 function validateNote(note) {
-  if(!note.title || typeof note.title !== "string") {
+  if (!note.title || typeof note.title !== "string") {
     return false;
   }
-  if(!note.text || typeof note.text !== "string") {
+  if (!note.text || typeof note.text !== "string") {
     return false;
   }
   return true;
-};
+}
 
 // post route that creates new notes  from user input
 router.post("/notes", (req, res) => {
   // sets unique id using uuid npm package
   req.body.id = uuidv4();
 
-  if(!validateNote(req.body)) {
+  if (!validateNote(req.body)) {
     res.status(400).send("The note is not properly formatted!");
   } else {
-    const note = createNewNote(req.body, notes);
-    res.json(notes);
+    const newNotes = createNewNote(req.body, notes);
+    res.json(newNotes);
   }
+  console.log("posted")
 });
 
-module.exports = router;    
+//function to delete note
+function deleteNote(ident, notesArray) {
+  for (let i = 0; i < notesArray.length; i++) {
+    if (notesArray[i].id === ident) {
+      notesArray.splice(i, 1);
+
+      fs.writeFileSync(
+        path.join(__dirname, "../../db/db.json"),
+        JSON.stringify({ notes: notesArray }, null, 2)
+      );
+
+      return notesArray;
+    }
+  }
+}
+
+//delete route that deletes notes
+router.delete("/notes/:id", (req, res) => {
+  const note = deleteNote(req.params.id, notes);
+
+  res.json(note);
+});
+
+module.exports = router;
